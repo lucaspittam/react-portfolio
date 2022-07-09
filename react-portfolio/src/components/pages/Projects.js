cc7ab379af94c3d603ac945afe794b487e197b17
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-07-08 21:04:34 -0400

    added resume and content on projects page, preliminary styling

--- react-portfolio/src/components/pages/Projects.js
+++ react-portfolio/src/components/pages/Projects.js
@@ -1,31 +1,51 @@
 import React from 'react';
 
+const projects = [
+    {
+        name: "Run Buddy",
+        liveURL: "https://lucaspittam.github.io/run-buddy/",
+        ghURL: "https://github.com/lucaspittam/run-buddy",
+        tech: "HTML/CSS",
+        style: "ecommerce"
+    },
+    
+    {
+        name: "Residential Matchmaker",
+        liveURL: "https://residential-matchmaker.herokuapp.com/",
+        ghURL: "https://github.com/Group-0007/Project-2-Group-7",
+        tech: "Node.js/Express/Sequelize/Handlebars",
+        style: "app1"
+    },
+    {
+        name: "Trekking-App",
+        liveURL: "https://project1team7.github.io/Trekking-App",
+        ghURL: "https://github.com/project1team7/Trekking-App",
+        tech: "Node.js/Express",
+        style: "app2"
+    },
+    {
+        name: "Techy blog",
+        liveURL: "https://protected-sea-89530.herokuapp.com/",
+        ghURL: "https://github.com/lucaspittam/Techy-blog",
+        tech: "Node.js/Express/",
+        style: "blog-site"
+    }
+]
 const Projects = () => (
     <div>
         <h1>Projects</h1>
-        <p>
-            Project 1
-            Project 1
-            Project 1
-            Project 1
-            Project 1
-            Project 1
-        </p>
-        <p>
-            Project 2
-            Project 2
-            Project 2
-            Project 2
-            Project 2
-            Project 2
-        </p>
-        <p>
-            Project 3
-            Project 3
-            Project 3
-            Project 3
-            Project 3
-        </p>
+            {projects.map(project => (
+                <div class={project.style}> 
+                    <a href={project.liveURL} target="_blank" rel="noopener noreferrer" key={project.name }>
+                        {project.name}
+                    </a>
+                    <a href={project.ghURL} target="_blank" rel="noopener noreferrer">
+                        GitHub Repo
+                    </a>
+                    <p>{project.tech}</p>
+                </div>
+                
+            ))}
     </div>
 );
 
--- react-portfolio/src/components/pages/Resume.js
+++ react-portfolio/src/components/pages/Resume.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import resume from "../../pics/LucasPittam_Resume_2022.pdf";
 
+const resumeName = resume.split('/')[3].split('.')[0].concat(".pdf");
+
+
+//get the div that has all of the info for Resume 
 const Resume = () => (
     <div>
-        <h1>Resume</h1>
-        <p>
-            Blah Blah school blah work
-        </p>
+        <a href={resume} download={resumeName}>Resume</a>
+
+        <h3>Front-end Skills</h3>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+            <li>responsive design</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+        </ul>
+
+        <h3>Back-end Skills</h3>
+        <ul>
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>REST</li>
+        </ul>
     </div>
 );
 
